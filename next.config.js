db24128c23c544b8563c090607d2cdb0de4c030f
Aleksander Steen <aleksander@selvklart.no>
2022-11-21 13:50:25 +0100

    Add editorconfig, eslint configuration and enable SWC and emotion support for SWC

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
+const nextConfig = {
+	reactStrictMode: true,
+  poweredByHeader: false,
+	swcMinify: true,
+  compiler: {
+    emotion: true
+  }
 };
+
+module.exports = nextConfig;
